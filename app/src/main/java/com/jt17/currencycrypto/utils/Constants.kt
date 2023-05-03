fdbc03628cf3f318d54990368b16bded2f6fb3a9
jahongir17tolibov <jahongir17tolibov@gmail.com>
2023-05-04 00:19:17 +0500

    #added constants

---
+++ app/src/main/java/com/jt17/currencycrypto/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.jt17.currencycrypto.utils
+
+object Constants {
+    const val LOG_TXT = "jt1771tj"
+    const val CURR_TXT = "currency"
+    const val CRY_TXT = "crypto"
+    const val CURRENCY_URL = "https://cbu.uz/uz/arkhiv-kursov-valyut/"
+    const val CRYPTO_URL = "https://api.coinlore.net/api/"
+    const val APP_VERSION = "1.0.1"
+    const val IMAGE_URL = "https://flagcdn.com/w160/"
+    const val IMAGE_CRYPTO_URL = "https://coinicons-api.vercel.app/api/icon/"
+}
