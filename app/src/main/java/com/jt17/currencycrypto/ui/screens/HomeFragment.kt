3da826f5e2fcc7b162e01cc6ca1d6dba1bbe9ad5
jahongir17tolibov <jahongir17tolibov@gmail.com>
2023-05-04 00:16:32 +0500

    #Added change theme using viewModel with stateFlow

--- app/src/main/java/com/jt17/currencycrypto/ui/screens/HomeFragment.kt
+++ app/src/main/java/com/jt17/currencycrypto/ui/screens/HomeFragment.kt
@@ -1,78 +1,86 @@
 package com.jt17.currencycrypto.ui.screens
 
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewAnimationUtils
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.airbnb.lottie.LottieDrawable
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.jt17.currencycrypto.R
-import com.jt17.currencycrypto.data.sharedPref.AppPreference
 import com.jt17.currencycrypto.databinding.FragmentHomeBinding
 import com.jt17.currencycrypto.ui.activities.MainActivity
+import com.jt17.currencycrypto.utils.Constants.LOG_TXT
+import com.jt17.currencycrypto.viewmodels.ThemeViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlin.math.pow
-import kotlin.math.sqrt
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
-
+class HomeFragment : Fragment(R.layout.fragment_home) {
     private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentHomeBinding
 
     private val navigation by lazy { findNavController() }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
-        return binding.root
+    private val viewModel by viewModels<ThemeViewModel>() {
+        ViewModelProvider.AndroidViewModelFactory(requireActivity().application)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
+        _binding = binding
 
         initClicks()
         setupAnimations()
 
     }
 
-    private fun changeTheme() {
-        val pos = when (!AppPreference.getInstance().loadNightModeState()) {
-            true -> {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                true
-            }
-            false -> {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                false
+    private val changeThemeViewModel = lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            viewModel.themeState.collect { isNightModeEnabled ->
+                changeTheme(isNightModeEnabled)
+                Log.d(LOG_TXT, "in started $isNightModeEnabled")
             }
         }
-        AppPreference.getInstance().setNightModeState(pos)
+    }
 
+    private fun changeTheme(state: Boolean) = when (state) {
+        true -> {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            true
+        }
+
+        false -> {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            false
+        }
     }
 
     private fun initClicks() {
         binding.changeTheme.setOnClickListener {
-            AppPreference.getInstance().loadNightModeState() != AppPreference.getInstance()
-                .loadNightModeState()
-            changeTheme()
-            requireActivity().recreate()
+            Log.d(LOG_TXT, "value in thread: ${!viewModel.themeState.value}")
+            viewModel.setThemeState(!viewModel.themeState.value)
+            changeThemeViewModel.runCatching {
+                requireActivity().recreate()
+                Log.d(LOG_TXT, "activity recreated!!!")
+            }
         }
 
         binding.favCurrenciespath.setOnClickListener {
-            navigation.navigate(R.id.favCurrenciesFragment)
+            val action = HomeFragmentDirections.actionHomeFragmentToFavCurrenciesFragment()
+            navigation.navigate(action)
         }
 
         binding.favCryptoPath.setOnClickListener {
-            navigation.navigate(R.id.favCryptoFragment)
+            val action = HomeFragmentDirections.actionHomeFragmentToFavCryptoFragment()
+            navigation.navigate(action)
         }
 
     }
---
+++ app/src/main/java/com/jt17/currencycrypto/viewmodels/ThemeViewModel.kt
@@ -0,0 +1,27 @@
+package com.jt17.currencycrypto.viewmodels
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.jt17.currencycrypto.data.sharedPref.AppPreference
+import com.jt17.currencycrypto.utils.Constants.LOG_TXT
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ThemeViewModel @Inject constructor(application: Application) : AndroidViewModel(application) {
+
+    private val _themeState = MutableStateFlow(AppPreference.getInstance().loadNightModeState())
+    val themeState: StateFlow<Boolean> get() = _themeState
+
+    fun setThemeState(state: Boolean) = viewModelScope.launch {
+        AppPreference.getInstance().setNightModeState(state)
+        _themeState.value = state
+        Log.d(LOG_TXT, "param: $state | value: ${_themeState.value}")
+    }
+
+}
